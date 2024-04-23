f10082056268dce00eb18fcddafd0806afad1b3d
Heller07 <144932413+Heller07@users.noreply.github.com>
2024-04-23 22:38:26 +0530

    Update next permutation.cpp

--- array/next permutation/next permutation.cpp
+++ array/next permutation/next permutation.cpp
@@ -9,3 +9,27 @@ public:
     }
 };
 #2
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int index = -1;//dip index
+        int n = nums.size();
+        for(int i = n-2;i>=0;i--){
+            if(nums[i]<nums[i+1]){
+                index = i;
+                break;
+            }
+        }
+        if(index ==-1){
+            reverse(nums.begin(),nums.end());
+            return;
+        }
+        for(int i = n-1;i>index;i--){
+            if(nums[index]<nums[i]){
+                swap(nums[i],nums[index]);
+            break;
+            }
+        }
+        reverse(nums.begin()+index+1,nums.end());
+    }
+};
