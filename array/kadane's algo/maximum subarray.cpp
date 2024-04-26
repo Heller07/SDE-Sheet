43f7252099752ffe939817ad30f63de74677ce1e
Heller07 <144932413+Heller07@users.noreply.github.com>
2024-04-26 14:27:22 +0530

    Update maximum subarray.cpp

--- array/kadane's algo/maximum subarray.cpp
+++ array/kadane's algo/maximum subarray.cpp
@@ -21,3 +21,71 @@ public:
     return maxi;
     }
 };
+#2
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxi = INT_MIN; // maximum sum
+        int n = nums.size();
+    for (int i = 0; i < n; i++) {
+            int sum = 0;
+        for (int j = i; j < n; j++) {
+            // subarray = arr[i.....j]
+
+            //add all the elements of subarray:
+           
+                sum += nums[j];
+            
+
+            maxi = max(maxi, sum);
+        }
+#3//optimal
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int maxi = INT_MIN; // maximum sum
+        int n = nums.size();
+        int sum = 0;
+    for (int i = 0; i < n; i++) {
+            sum += nums[i];
+            if(sum>maxi){
+                maxi = sum;
+            }
+            if(sum<0){
+                sum = 0;
+            }
+        
+    }
+
+    return maxi;
+    
+}
+};
+//if we have to print the max sub array
+//so we just need the index of start and end
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+      int start,anstart,ansend;
+        int maxi = INT_MIN; // maximum sum
+        int n = nums.size();
+        int sum = 0;
+    for (int i = 0; i < n; i++) {
+      if(sum==0) start = i;
+            sum += nums[i];
+            
+            if(sum>maxi){
+                maxi = sum;
+                anstart = start;
+                ansend = i;
+            }
+            if(sum<0){
+                sum = 0;
+            }
+        
+    }
+
+    return maxi;
+    
+}
+};
