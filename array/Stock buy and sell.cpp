4bbc20a70e8d41ede29a18026fdd09cbedbc2322
Heller07 <144932413+Heller07@users.noreply.github.com>
2024-04-26 16:49:36 +0530

    Create Stock buy and sell.cpp

---
+++ array/Stock buy and sell.cpp
@@ -0,0 +1,30 @@
+//1
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxi = INT_MIN;
+        int sub = 0;
+        for(int i = 0;i<prices.size();i++){
+            for(int j = i;j<prices.size();j++){//we can start it by i+1
+                if(prices[j]>prices[i]){
+                sub = prices[j]-prices[i];
+            }
+                maxi = max(maxi,sub);
+            }
+        }
+        return maxi;
+    }
+};
+//2
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxi = INT_MIN;
+        int mini = INT_MAX;
+        for(int i = 0;i<prices.size();i++){
+            mini = min(mini,prices[i]);
+            maxi = max(maxi,prices[i]-mini);
+        }
+        return maxi;
+    }
+};
