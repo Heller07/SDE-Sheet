fc1e8eb1d574fb71720ca01dffb80c412fa730bc
Heller07 <144932413+Heller07@users.noreply.github.com>
2024-04-26 16:55:41 +0530

    Update Stock buy and sell.cpp

--- array/Stock buy and sell.cpp
+++ array/Stock buy and sell.cpp
@@ -19,7 +19,7 @@ public:
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int maxi = INT_MIN;
+        int maxi = 0;
         int mini = INT_MAX;
         for(int i = 0;i<prices.size();i++){
             mini = min(mini,prices[i]);
