11d1922fa16411034d826e875f940d3ac06507e0
Heller07 <144932413+Heller07@users.noreply.github.com>
2024-04-26 17:01:57 +0530

    Update Stock buy and sell.cpp

--- array/Stock buy and sell.cpp
+++ array/Stock buy and sell.cpp
@@ -22,8 +22,8 @@ public:
         int maxi = 0;
         int mini = INT_MAX;
         for(int i = 0;i<prices.size();i++){
-            mini = min(mini,prices[i]);
-            maxi = max(maxi,prices[i]-mini);
+            mini = min(mini,prices[i]);//minimising the buying stock
+            maxi = max(maxi,prices[i]-mini);//maximizing the profit
         }
         return maxi;
     }
