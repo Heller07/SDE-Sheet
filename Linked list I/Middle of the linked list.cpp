cc8ccfdd0b472cf84ef26cc28f406b6aeaa49926
Heller07 <144932413+Heller07@users.noreply.github.com>
2024-06-11 13:44:47 +0530

    Create Middle of the linked list.cpp

---
+++ Linked list I/Middle of the linked list.cpp
@@ -0,0 +1,59 @@
+//1
+// Time Complexity: O(N+N/2) The code traverses the entire linked list once and half times and then only half in the second iteration, first to count the number of nodes then then again to get to the middle node. Therefore, the time complexity is linear, O(N + N/2) ~ O(N).
+// Space Complexity : O(1) There is constant space complexity because it uses a constant amount of extra space regardless of the size of the linked list. We only use a few variables to keep track of the middle position and traverse the list, and the memory required for these variables does not depend on the size of the lis
+Node *findMiddle(Node *head) {
+    // If the list is empty or has only
+    // one element, return the head as
+    // it's the middle.
+    if (head == NULL || head->next == NULL) {
+        return head;
+    }
+
+    Node* temp = head;
+    int count = 0;
+
+    // Count the number of nodes in the linked list.
+    while (temp != NULL) {
+        count++;
+        temp = temp->next;
+    }
+
+    // Calculate the position of the middle node.
+    int mid = count / 2 + 1;
+    temp = head;
+
+    // Traverse to the middle node by moving
+    // temp to the middle position.
+    while (temp != null) {
+        mid = mid - 1;
+
+        // Check if the middle
+        // position is reached.
+        if (mid == 0){
+            // break out of the loop
+            // to return temp
+            break;
+        }
+        // Move temp ahead
+        temp = temp->next;
+    }
+
+    // Return the middle node.
+    return temp;
+}
+
+//2
+//Time Complexity: O(N/2) The algorithm requires the 'fast' pointer to reach the end of the list which it does after approximately N/2 iterations (where N is the total number of nodes). Therefore, the maximum number of iterations needed to find the middle node is proportional to the number of nodes in the list, making the time complexity linear, or O(N/2) ~ O(N).\
+// Space Complexity : O(1) There is constant space complexity because it uses a constant amount of extra space regardless of the size of the linked list. We only use a few variables to keep track of the middle position and traverse the list, and the memory required for these variables does not depend on the size of the list.
+class Solution {
+public:
+    ListNode* middleNode(ListNode* head) {
+        ListNode* fast = head;
+        ListNode* slow = head;
+        while(fast != NULL && fast->next != NULL){
+            fast = fast->next->next;
+            slow = slow->next;
+        }
+        return slow;
+    }
+};
