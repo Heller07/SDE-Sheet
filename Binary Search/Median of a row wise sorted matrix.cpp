81630a810e1e48a19e74a5558a24d049a98073a9
Heller07 <144932413+Heller07@users.noreply.github.com>
2024-06-17 19:09:00 +0530

    Create Median of a row wise sorted matrix.cpp

---
+++ Binary Search/Median of a row wise sorted matrix.cpp
@@ -0,0 +1,78 @@
+// Time Complexity: O(MXN) + O(MXN(log(MXN))), where M = number of row in the given matrix, N = number of columns in the given matrix
+// Reason: At first, we are traversing the matrix to copy the elements. This takes O(MXN) time complexity. Then we are sorting the linear array of size (MXN), that takes O(MXN(log(MXN))) time complexity
+// Space Complexity: O(MXN) as we are using a temporary list to store the elements of the matrix.
+int func(int mid, int n, int m) {
+    long long ans = 1;
+    for (int i = 1; i <= n; i++) {
+        ans = ans * mid;
+        if (ans > m) return 2;
+    }
+    if (ans == m) return 1;
+    return 0;
+}
+
+int NthRoot(int n, int m) {
+    //Use Binary search on the answer space:
+    int low = 1, high = m;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int midN = func(mid, n, m);
+        if (midN == 1) {
+            return mid;
+        }
+        else if (midN == 0) low = mid + 1;
+        else high = mid - 1;
+    }
+    return -1;
+}
+// Time Complexity: O(log(10^9)) X O(M(logN)), where M = number of rows in the given matrix, N = number of columns in the given matrix
+// Reason: Our search space lies between [0, 10^9] as the min(matrix) can be 0 and the max(matrix) can be 10^9. We are applying binary search in this search space and it takes O(log(109)) time complexity. Then we call countSmallEqual() function for every ‘mid’ and this function takes O(M(logN)) time complexity.
+// Space Complexity : O(1) as we are not using any extra space
+class Solution{   
+public:
+    int upperBound(vector<int> &arr, int x, int n) {
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        // maybe an answer
+        if (arr[mid] > x) {
+            ans = mid;
+            //look for smaller index on the left
+            high = mid - 1;
+        }
+        else {
+            low = mid + 1; // look on the right
+        }
+    }
+    return ans;
+}
+
+int countSmallEqual(vector<vector<int>> &matrix, int m, int n, int x) {
+    int cnt = 0;
+    for (int i = 0; i < m; i++) {
+        cnt += upperBound(matrix[i], x, n);
+    }
+    return cnt;
+}
+
+int median(vector<vector<int>> &matrix, int m, int n) {
+    int low = INT_MAX, high = INT_MIN;
+
+    //point low and high to right elements:
+    for (int i = 0; i < m; i++) {
+        low = min(low, matrix[i][0]);
+        high = max(high, matrix[i][n - 1]);
+    }
+
+    int req = (n * m) / 2;
+    while (low <= high) {
+        int mid = (low + high) / 2;
+        int smallEqual = countSmallEqual(matrix, m, n, mid);
+        if (smallEqual <= req) low = mid + 1;
+        else high = mid - 1;
+    }
+    return low;
+}
+};
