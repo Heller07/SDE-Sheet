3061fbc08f784c09d0d14fc5d23a9b74193850ec
Heller07 <144932413+Heller07@users.noreply.github.com>
2024-07-13 19:02:22 +0530

    Create Mirror Tree.cpp

---
+++ Binary Tree III/Mirror Tree.cpp
@@ -0,0 +1,16 @@
+//O(N)
+//O(H)
+class Solution {
+  public:
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node* node) {
+        // code here
+        if(node == NULL) return;
+        
+        mirror(node->left);
+        mirror(node->right);
+        Node*temp = node->right;
+        node->right = node->left;
+        node->left = temp;
+    }
+};
