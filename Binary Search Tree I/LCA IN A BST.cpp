e27115175e7245b3bad1ded7995cd1fc7976d8bd
Heller07 <144932413+Heller07@users.noreply.github.com>
2024-07-14 23:17:36 +0530

    Create LCA IN A BST.cpp

---
+++ Binary Search Tree I/LCA IN A BST.cpp
@@ -0,0 +1,27 @@
+//TC is O(N)//SC IS O(H) recursive stack space
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL) return NULL;
+        if(root->val == p->val || root->val == q->val) return root;
+
+        TreeNode*left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
+
+        if(!left && !right) return NULL;
+        if(!left && right) return right;
+        if(!right && left) return left;
+        return root;
+    }
+};
+
+//O(H)(not skew tree)//SC IS SAME AS ABOVE
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL) return NULL;
+        if((root->val > p->val) && (root->val > q->val)) return lowestCommonAncestor(root->left,p,q);
+        if((root->val < p->val) && (root->val < q->val)) return lowestCommonAncestor(root->right,p,q);
+        return root;
+    }
+};
