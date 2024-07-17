e5ed93ef5a8d7fe136fe3c8e530a001def1e8217
Heller07 <144932413+Heller07@users.noreply.github.com>
2024-07-17 13:08:44 +0530

    Update Largest BST in a Binary Tree.cpp

--- Binary Search Tree II/Largest BST in a Binary Tree.cpp
+++ Binary Search Tree II/Largest BST in a Binary Tree.cpp
@@ -1,3 +1,40 @@
+//bruute
+//TC is O(n2)
+class Solution {
+public:
+    bool isbst(TreeNode* root, int lv, int rv)
+    {
+        if(!root)return true;
+        if(root->val<=lv || root->val>=rv)return false;
+
+        return isbst(root->left, lv, root->val) && isbst(root->right, root->val, rv);
+    }
+
+    int findSum(TreeNode* root)
+    {
+        if(!root)return 0;
+        if(!root->left && !root->right)return root->val;
+        return root->val + findSum(root->left)+ findSum(root->right);
+    }
+
+    int find(TreeNode* root)
+    {
+        if(!root)return 0;
+        if(!root->left && !root->right)return root->val;
+        int rootbstsum=0;
+        if(isbst(root, INT_MIN, INT_MAX))
+        {
+            rootbstsum =  findSum(root);
+        }
+        int leftbstsum = max(rootbstsum,find(root->left));
+        int rightbstsum = max(leftbstsum, find(root->right));
+        return max(0, rightbstsum);
+    }
+    int maxSumBST(TreeNode* root) {
+        return find(root);
+    }
+};
+
 //TC is O(n)
 //SC is O(1)
 class Info {
