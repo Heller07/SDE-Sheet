d3dc2c11a8dd2b462da953a9c76f9e6dcff529af
Heller07 <144932413+Heller07@users.noreply.github.com>
2024-07-28 20:31:33 +0530

    Create Toposort using BFS.cpp

---
+++ Graph I/Toposort using BFS.cpp
@@ -0,0 +1,33 @@
+// Time Complexity: O(N+E)
+// Space complexity: O(N)+O(N)
+class Solution {
+public:
+	vector<int> topo(int N, vector<int> adj[]) {
+        queue<int> q; 
+	    vector<int> indegree(N, 0); 
+	    for(int i = 0;i<N;i++) {
+	        for(auto it: adj[i]) {
+	            indegree[it]++; 
+	        }
+	    }
+	    
+	    for(int i = 0;i<N;i++) {
+	        if(indegree[i] == 0) {
+	            q.push(i); 
+	        }
+	    }
+	    vector<int> topo;
+	    while(!q.empty()) {
+	        int node = q.front(); 
+	        q.pop(); 
+	        topo.push_back(node);
+	        for(auto it : adj[node]) {
+	            indegree[it]--;
+	            if(indegree[it] == 0) {
+	                q.push(it); 
+	            }
+	        }
+	    }
+	    return topo;
+	}
+};
