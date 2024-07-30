1098a6664c4c49ac482b0ab43cf77cdb6b3a342e
Heller07 <144932413+Heller07@users.noreply.github.com>
2024-07-30 15:15:01 +0530

    Create Floyd warshall.cpp

---
+++ Graph II/Floyd warshall.cpp
@@ -0,0 +1,37 @@
+// Time Complexity: O(V3), as we have three nested loops each running for V times, where V = no. of vertices.
+// Space Complexity: O(V2), where V = no. of vertices. This space complexity is due to storing the adjacency matrix of the given graph
+//used for negative edges if we want to find shortest path from all edges,though we can apply dijkstra for every node which have better timecomplexity but it does not valid for negative cycles thats why we use this
+class Solution {
+  public:
+	void shortest_distance(vector<vector<int>>&matrix) {
+		int n = matrix.size();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (matrix[i][j] == -1) {
+					matrix[i][j] = 1e9;
+				}
+				if (i == j) matrix[i][j] = 0;
+			}
+		}
+
+		for (int k = 0; k < n; k++) {
+			for (int i = 0; i < n; i++) {
+				for (int j = 0; j < n; j++) {
+					matrix[i][j] = min(matrix[i][j],
+					                   matrix[i][k] + matrix[k][j]);
+				}
+			}
+		}
+
+
+
+
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n; j++) {
+				if (matrix[i][j] == 1e9) {
+					matrix[i][j] = -1;
+				}
+			}
+		}
+	}
+};
